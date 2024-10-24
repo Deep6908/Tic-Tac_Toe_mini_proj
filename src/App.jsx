10ab163ab800c28958721db2b62090b8967af972
Deep6908 <dipakpaul637@gmail.com>
2024-10-24 21:22:04 +0530

    New Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Grid from './components/Grid/Grid'
+
+function App() {
+  
+  return (
+    <>
+      <Grid numberofCards={9} />
+    </>
+  )
+}
+
+export default App
